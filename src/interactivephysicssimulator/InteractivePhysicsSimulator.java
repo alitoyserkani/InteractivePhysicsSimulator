7f220fe7c91df43636f30c3b36a3c1524d7c1e22
alitoyserkani <alitoyserkani@172.21.138.138>
2016-09-13 23:28:26 -0400

    GUI working with physical properties

---
+++ src/interactivephysicssimulator/InteractivePhysicsSimulator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interactivephysicssimulator;
+
+import java.util.ArrayList;
+
+public class InteractivePhysicsSimulator { //starts off program
+    
+
+    public static void main(String[] args) {
+        SimulatorGUI sim = new SimulatorGUI();
+        sim.setTitle("Interactive Physics Simulator");
+        sim.setVisible(true);
+        
+        
+       
+    }
+}
